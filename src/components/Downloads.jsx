1ca063f92a15265a4a6c0a69f34d75cd205919ba
MD. YEADH HASAN BAPPY <yeadhapps@gmail.com>
2023-04-01 05:35:16 +0600

    add download directly update

--- src/components/Downloads.jsx
+++ src/components/Downloads.jsx
@@ -71,10 +71,10 @@ class Downloads extends React.Component {
                   <h4 className="new_title_bold">closed beta now!</h4>
                   <h2 className="download-now">Download Now : </h2>
                   <div className="download-btn-wrap">
-                    <a target="_blank" rel="noreferrer" href="https://launcher.katanainu.com/">
+                    <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
                       <img src="assets/img/images/download_btn.png" alt="" />
                     </a>
-                    <a target="_blank" rel="noreferrer" href="https://launcher.katanainu.com/">
+                    <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
                       <img src="assets/img/images/play.png" alt="" />
                     </a>
                     <a className='white' target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
@@ -261,10 +261,10 @@ class Downloads extends React.Component {
                 <div className="newplay_area mt-50">
                   <h2 className="download-now">Download Now : </h2>
                   <div className="download-btn-wrap">
-                    <a target="_blank" rel="noreferrer" href="https://launcher.katanainu.com/">
+                    <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
                       <img src="assets/img/images/download_btn.png" alt="" />
                     </a>
-                    <a target="_blank" rel="noreferrer" href="https://launcher.katanainu.com/">
+                    <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
                       <img src="assets/img/images/play.png" alt="" />
                     </a>
                     <a className='white' target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1gwRleTzjZad5Ktp8YCjr3FgYPqKGRo1v/view">
